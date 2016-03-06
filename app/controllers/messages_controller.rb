6372525c6155c9f48371fd939c997a3e0e0a5f05
wggsh <is0332ks.ed@ritsumei.ac.jp>
2016-03-06 07:03:23 +0000

    change Gem file for Heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,8 +6,18 @@ class MessagesController < ApplicationController
     
     def create
       @message=Message.new(message_params)
-      @message.save
-      redirect_to root_path,notice:'メッセージを保存しました'
+      if @message.save
+        redirect_to root_path,notice:'メッセージを保存しました'
+      else
+        @message=Message.all
+        redirect_to root_path,notice:'メッセージの保存に失敗しました'
+      end
+    end
+    
+    def destroy
+      @message = Message.find(params[:id])
+      @message.destroy
+      redirect_to root_path, notice: 'メッセージを削除しました'
     end
     
     private
